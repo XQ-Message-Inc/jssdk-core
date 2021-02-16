4c9cbae30a238f1ccc65215f4440461294a9f45c
XQ Message <social@xqmsg.com>
2021-02-16 01:14:43 -0500

    Update configuration file.

---
+++ src/com/xqmsg/sdk/v2/Config.js
@@ -0,0 +1,10 @@
+export default class Config {
+    constructor() {
+        this.application = {
+            API_KEY: "",
+            SUBSCRIPTION_SERVER_URL : 'https://subscription.xqmsg.net/v2',
+            VALIDATION_SERVER_URL : 'https://validation.xqmsg.net/v2',
+            KEY_SERVER_URL : 'https://quantum.xqmsg.net/v2/'
+        }
+    }
+}
