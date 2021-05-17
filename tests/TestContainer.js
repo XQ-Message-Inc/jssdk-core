51f952a70c8cafadb7599b081c6d8ff0d6b17062
jan <jan@xqmsg.com>
2021-05-17 15:16:43 +0700

    Revert "Revert "renamed a few files""
    This reverts commit f6b8adc8

--- tests/TestContainer.js
+++ tests/TestContainer.js
@@ -37,9 +37,9 @@ import CodeValidator from "../src/com/xqmsg/sdk/v2/services/CodeValidator.js";
 
 /**
  * This class contains the tests.
- * @class [Agenda]
+ * @class [TestContainer]
  */
-export default class Agenda {
+export default class TestContainer {
 
     constructor(aSdk, aSamplerFileContent) {
         this.xqsdk = aSdk;
