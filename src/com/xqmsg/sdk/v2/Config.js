21f26adca2f1a48626853ef6a15f46ee4bba1f45
jan <jan@xqmsg.com>
2021-05-17 15:21:23 +0700

    Config update

--- src/com/xqmsg/sdk/v2/Config.js
+++ src/com/xqmsg/sdk/v2/Config.js
@@ -4,22 +4,12 @@
 export default class Config {
     constructor() {
         this.application = {
-            // STAGING
 
-            // API_KEY: 'a1a03794-33e0-4e01-8030-36052e1a55ed-4eb8e457-0f15-42f4-b398-4ce038687cfb',
-            // SUBSCRIPTION_SERVER_URL : 'https://stg-subscription.xqmsg.net/v2',
-            // VALIDATION_SERVER_URL : 'https://stg-validation.xqmsg.net/v2',
-            // KEY_SERVER_URL : 'https://quantum.xqmsg.net/v2/',
-            // DASHBOARD_API_KEY: '',
-            // DASHBOARD_SERVER_URL : ''
-
-            // PROD
-
-            XQ_API_KEY: '9ae6aa34-4d9f-421a-8337-0a0ccf62dc28-f1c91468-702f-4a0f-a9a6-d4c906d78c21',
+            XQ_API_KEY: 'YOUR_XQ_API_KEY',
             SUBSCRIPTION_SERVER_URL : 'https://subscription.xqmsg.net/v2',
             VALIDATION_SERVER_URL : 'https://validation.xqmsg.net/v2',
             KEY_SERVER_URL : 'https://quantum.xqmsg.net/v2/',
-            DASHBOARD_API_KEY: 'b43cefa1-b75b-4152-8ebd-4bf982f71e23-eb81b188-2c25-4d75-8375-beed88a76e2b',
+            DASHBOARD_API_KEY: 'YOUR_DASHBOARD_API_KEY',
             DASHBOARD_SERVER_URL : 'https://dashboard.xqmsg.net/v2'
         }
     }
