6e6ecc979a6e1b0e4bd3e578629460f78c6a5ac0
Blake Estes <blake.estes@protonmail.com>
2021-05-17 19:39:08 -0600

    rm API_KEY

--- src/com/xqmsg/sdk/v2/Config.js
+++ src/com/xqmsg/sdk/v2/Config.js
@@ -4,8 +4,7 @@
 export default class Config {
   constructor() {
     this.application = {
-      XQ_API_KEY:
-        "5e9b3574-c3ec-419d-b2b6-ba3d0a900562-b817b4dc-6961-48ac-95a4-8a32463d9fbc",
+      XQ_API_KEY: "",
       SUBSCRIPTION_SERVER_URL: "https://subscription.xqmsg.net/v2",
       VALIDATION_SERVER_URL: "https://validation.xqmsg.net/v2",
       KEY_SERVER_URL: "https://quantum.xqmsg.net/v2/",
