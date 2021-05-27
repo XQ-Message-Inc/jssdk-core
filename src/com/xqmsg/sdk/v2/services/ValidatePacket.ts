7ca39d007df7c32962623d03fffdccd19e92ae91
Ike E <social@xqmsg.com>
2021-05-27 15:46:51 -0400

    Merge Fixes

--- src/com/xqmsg/sdk/v2/services/ValidatePacket.ts
+++ src/com/xqmsg/sdk/v2/services/ValidatePacket.ts
@@ -4,8 +4,8 @@ import XQModule from "./XQModule";
 import XQSDK from "../XQSDK";
 
 /**
- *  Validates the packet that is returned from {@link GeneratePacket}.<br>
- *  Returns the key locator token<.br>
+ *  A service which is utilized to validate a {@link Authorize} service call
+ *  Returns 204, "No Content" if successful.
  *  @class [ValidatePacket]
  */
 export default class ValidatePacket extends XQModule {
@@ -33,11 +33,6 @@ export default class ValidatePacket extends XQModule {
     this.serviceName = "packet";
     this.requiredFields = [ValidatePacket.PACKET];
 
-    /**
-     *
-     * @param {{}} maybePayLoad:
-     * @returns {Promise<ServerResponse<{payload:String}>>} the server response containing the key locator token
-     */
     this.supplyAsync = (maybePayLoad) => {
       try {
         this.sdk.validateInput(maybePayLoad, this.requiredFields);
@@ -70,5 +65,3 @@ export default class ValidatePacket extends XQModule {
     };
   }
 }
-
-ValidatePacket.PACKET = "data";
