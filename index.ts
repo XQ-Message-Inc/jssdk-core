315367be1145a1bb6033484d1d44d846d31361e3
Blake Estes <blake.estes@protonmail.com>
2021-05-28 13:25:16 -0600

    switch to webpack

---
+++ index.ts
@@ -0,0 +1,5 @@
+import * as jssdkcore from "./src/";
+
+export * from "./src/";
+
+export default jssdkcore;
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "production",
+  entry: "./index.ts",
+  output: {
+    filename: "jssdk-core.min.js",
+    path: path.resolve(__dirname, "dist/browser"),
+    library: {
+      type: "umd",
+      name: "jssdk-core",
+    },
+  },
+  devtool: "source-map",
+  resolve: {
+    // Add '.ts' as resolvable extensions
+    extensions: [".ts", ".js"],
+
+    // Support `path` in the browser
+    fallback: {
+      path: require.resolve("path-browserify"),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ],
+  module: {
+    rules: [
+      // All files with a '.ts' extension will be handled by 'ts-loader'.
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        options: {
+          configFile: path.resolve(__dirname, "tsconfig-browser.json"),
+        },
+      },
+
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: "source-map-loader" },
+    ],
+    // Don't parse tweetnacl module — https://github.com/dchest/tweetnacl-js/wiki/Using-with-Webpack
+    noParse: [/[\\/]tweetnacl[\\/]/, /[\\/]tweetnacl-auth[\\/]/],
+  },
+};
