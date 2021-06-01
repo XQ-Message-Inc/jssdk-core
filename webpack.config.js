758b5f9505e9c7764093ca99e243b753c72dcf0c
Blake Estes <blake.estes@protonmail.com>
2021-06-01 09:19:41 -0600

    rm noParse for non-existent tweetnacl library

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,5 @@ module.exports = {
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { test: /\.js$/, loader: "source-map-loader" },
     ],
-    // Don't parse tweetnacl module — https://github.com/dchest/tweetnacl-js/wiki/Using-with-Webpack
-    noParse: [/[\\/]tweetnacl[\\/]/, /[\\/]tweetnacl-auth[\\/]/],
   },
 };
